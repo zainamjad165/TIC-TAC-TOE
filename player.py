8efc0558f278232db5b462fe5c1a8a49f77bce9c
zainamjad165 <98817942+zainamjad165@users.noreply.github.com>
2022-02-19 16:18:06 +0500

    Add files via upload

---
+++ player.py
@@ -0,0 +1,35 @@
+
+import math
+import random
+
+class Player:
+    
+    def __init__(self, letter):
+        self.letter = letter
+
+class RandomComputerPlayer(Player):
+    
+    def __inint__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+   
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        valid_square = Falseval = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again.")
+        return val
